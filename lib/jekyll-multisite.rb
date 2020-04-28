sumdog/jekyll-multisite

commit 5b3dc16f46aeb618030140391ec015251a3e701e

Make sure we stay in the site folder
If a subfolder A of a site folder is named like a folder B in the root (/), then the folder B is taken instead of A.

Signed-off-by: Sven Strickroth <email@cs-ware.de>

lib/jekyll-multisite.rb
@@ -34,7 +34,7 @@ module Jekyll
        base_directory
      elsif questionable_path.start_with?(base_directory)
        questionable_path
-     elsif File.exists?(questionable_path) and questionable_path != '/'
+     elsif File.exists?(questionable_path) and !questionable_path.start_with?('/')
        File.expand_path(questionable_path)
      else
        File.join(base_directory, questionable_path)

commit a822ff417d0203a2df78039a8c06f1eb0b8b42ae

Make work on *nix and Windows
Fixes a regression introduced in commit 5b3dc16f46aeb618030140391ec015251a3e701e.

Signed-off-by: Sven Strickroth <email@cs-ware.de>

lib/jekyll-multisite.rb
@@ -34,7 +34,9 @@ module Jekyll
        base_directory
      elsif questionable_path.start_with?(base_directory)
        questionable_path
-     elsif File.exists?(questionable_path) and !questionable_path.start_with?('/')
+     elsif File.exists?(questionable_path) and !questionable_path.start_with?('/') and (ENV['OS'] == 'Windows_NT')
+       File.expand_path(questionable_path)
+     elsif File.exists?(questionable_path) and questionable_path != '/' and !(ENV['OS'] == 'Windows_NT')
        File.expand_path(questionable_path)
      else
        File.join(base_directory, questionable_path)